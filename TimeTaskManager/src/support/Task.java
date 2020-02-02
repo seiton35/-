db5769b23557dc0c40975d2564556eb4031b265e
seiton35 <prosto99-00@mail.ru>
2020-02-03 00:28:56 +0300

    тайм таск менеджер по системе pomodoro

---
+++ TimeTaskManager/src/support/Task.java
@@ -0,0 +1,60 @@
+package support;
+
+
+public class Task {
+
+    private int num;
+    private String name;
+    private String backlog;
+    private Boolean complete;
+
+    public Task(int num, String name, String backlog, Boolean complete) {
+        this.num = num;
+        this.name = name;
+        this.backlog = backlog;
+        this.complete = complete;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBacklog() {
+        return backlog;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBacklog(String backlog) {
+        this.backlog = backlog;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return "Task " + num +
+                "[ " + name +
+                ", steps( " + backlog +
+                " ) complete: " + complete +
+                " ]";
+    }
+}
