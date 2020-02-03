e568d60b85a44dde8a306f71d76f39c631733ebf
seiton35 <prosto99-00@mail.ru>
2020-02-04 00:56:30 +0300

    добавил справочник и начал обработку вводимых команд

--- TimeTaskManager/src/com/company/Main.java
+++ TimeTaskManager/src/com/company/Main.java
@@ -9,6 +9,12 @@ public class Main {
 
     static Task[] task = new Task[20];
 
+    static String help = "new - new task\n" +
+            "start([num of task]) - start timer for task [num of task]\n" +
+            "stop - stop timer\n" +
+            "complete([num of task]) - mark as completed\n" +
+            "delete([num of task]) - delete task [num of task]";
+
     public static void main(String[] args) {
         inputTask();
 
@@ -43,18 +49,29 @@ public class Main {
         if(name.length()!= 0) {
             System.out.print("Input a steps: ");
             steps = in.nextLine();
-            if(steps.length()!= 0) {
-
-            }else {
+            if(steps.length()== 0) {
                 System.out.println("must be at least one step!");
+                return;
             }
         }else {
             System.out.println("name is too short!");
+            return;
         }
         in.close();
         task[lastTask] = new Task(lastTask + 1, name, steps, false);
 
         printTasks();
+    }
+
+    private static void command(){
+        Scanner in = new Scanner(System.in);
+        String com = in.nextLine();
+        switch (com){
+            case ("help"):
+                System.out.println(help);
+                break;
+                case ("")
+        }
     }
 
 
